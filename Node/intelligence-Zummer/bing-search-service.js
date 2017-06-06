d51ee4037777a567a851b41e6cdaad736106bf4c
kandgargi <kandgargi@gmail.com>
2017-06-06 08:57:01 -0700

    Update bing-search-service.js

--- Node/intelligence-Zummer/bing-search-service.js
+++ Node/intelligence-Zummer/bing-search-service.js
@@ -1,9 +1,9 @@
 ﻿var apiHandler = require('./api-handler-service');
 
 const BING_SEARCH_API_URL = "https://api.cognitive.microsoft.com/bing/v5.0/search/",
-    BING_SEARCH_API_KEY = process.env.BING_SEARCH_API_KEY;
+    BING_SEARCH_API_KEY = process.env.a8ba95c4c0a74e2c934f8c0aa59baec;
 
-var headers = { "Ocp-Apim-Subscription-Key": BING_SEARCH_API_KEY }
+var headers = { "Ocp-Apim-Subscription-Key": a8ba95c4c0a74e2c934f8c0aa59baec }
 
 module.exports = {
     findArticles: (query) => {
